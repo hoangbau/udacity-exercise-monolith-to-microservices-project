358788aca2b0fde18592c13f7a38a8dd74dcb693
Victor Dam <victordamitpro@gmail.com>
2023-11-04 18:29:03 +0700

    update env

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
-export JWT_SECRET=testing
+export POSTGRES_PASSWORD=abcde123456-
+export POSTGRES_HOST=mypsql2023.c9pmxaoxlncc.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=testdb
+export AWS_BUCKET=arn:aws:s3:::mys3-dev-150170199826
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hoangdt
 export URL=http://localhost:8100
